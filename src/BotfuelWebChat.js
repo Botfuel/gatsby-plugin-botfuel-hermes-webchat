d9b3ceb6cccc16b631831ccd6eb92c7e58a225f9
Kevin Depuydt <kevin.dpdt@gmail.com>
2020-12-04 17:47:27 +0100

    update installation file

--- src/BotfuelWebChat.js
+++ src/BotfuelWebChat.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
-const BotfuelWebChat = (pluginOptions) => {
-  if (
-    pluginOptions.websiteId
-  ) {
+const BotfuelWebChat = ({ websiteId, ...otherSettings }) => {
+  if (websiteId) {
     return (
       <>
+        {Object.keys(otherSettings).length > 0 && (
+          <script
+            key="gatsby-plugin-botfuel-hermes-webchat-settings"
+            dangerouslySetInnerHTML={{
+              __html: `window.botfuelSettings = ${otherSettings};`
+            }}
+          />
+        )}
         <script
-          key={`gatsby-plugin-botfuel-hermes-webchat-js`}
-          src="https://cdn.jsdelivr.net/npm/botfuel-hermes-webchat-client"
-        />
-        <script
-          key={`gatsby-plugin-botfuel-hermes-webchat`}
+          key="gatsby-plugin-botfuel-hermes-webchat-widget"
           dangerouslySetInnerHTML={{
-            __html: `BotfuelWebChat.init({ websiteId: '${pluginOptions.websiteId}' });`,
+            __html: `<script>(function(){var id="${websiteId}",w=window,d=document;try{w.botfuelSettings.websiteId=id}catch(e){w.botfuelSettings={websiteId:id}}function l(){var s=d.createElement("script");s.type="text/javascript";s.async=!0;s.src="https://d1qsqz7hmkju8m.cloudfront.net/index.js";var t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t)}if(void 0===w.Botfuel){w.botfuelEventsQueue=[];w.Botfuel={track:w.botfuelEventsQueue.push};if(w.attachEvent){w.attachEvent("onload",l)}else{w.addEventListener("load",l,!1)}}})();</script>`,
           }}
         />
       </>
